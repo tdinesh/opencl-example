a325ae90c694e405b8aa1fba55870d84c0124c87
Fouassier Raphaël <ntimeu@ntimeu.fr>
2016-09-21 21:41:45 +0200

    Adding licensing informations + README.

--- cmake/Modules/FindOpenCL.cmake
+++ cmake/Modules/FindOpenCL.cmake
@@ -1,3 +1,10 @@
+# Simple tool to find the OpenCL library.
+#
+# Should not be used in full production setups.
+# Add this package to your cmake system module directory (or per-project)
+# Use the command target_link_libraries to add ${OPENCL_LIBRARY} to your project
+# to add -lOpenCL to your project.
+
 find_library(OPENCL_LIBRARY OpenCL)
 find_path(OPENCL_INCLUDE_DIR CL/cl.hpp)
 
