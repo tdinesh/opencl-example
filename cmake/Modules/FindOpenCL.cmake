a47865a385fab7ec6d6e7a5f888aa570073ae621
Raphaël FOUASSIER <ntimeu@users.noreply.github.com>
2016-10-26 11:08:47 +0200

    Various project improvements. (#1)
    Improve moduke finding reporting. Fail on error.
    Project directive now contains the C++ language adviser.
    Make will fail if the user is intending to cast with the old C-style.
    Show user the fact that we are using CCache or not (module ?).
    Lower Boost version requirements.

--- cmake/Modules/FindOpenCL.cmake
+++ cmake/Modules/FindOpenCL.cmake
@@ -5,8 +5,22 @@
 # Use the command target_link_libraries to add ${OPENCL_LIBRARY} to your project
 # to add -lOpenCL to your project.
 
+message(STATUS "Looking for OpenCL headers and library.")
+
 find_library(OPENCL_LIBRARY OpenCL)
 find_path(OPENCL_INCLUDE_DIR CL/cl.hpp)
+
+if(OPENCL_LIBRARY-NOTFOUND)
+    message(FATAL_ERROR "OpenCL runtime not found on your platform.")
+else(OPENCL_LIBRARY)
+    message(STATUS "Found OpenCL runtime.")
+endif(OPENCL_LIBRARY-NOTFOUND)
+
+if(OPENCL_INCLUDE_DIR-NOTFOUND)
+    message(FATAL_ERROR "OpenCL headers not found on your platform.")
+else(OPENCL_INCLUDE_DIR)
+    message(STATUS "Found OpenCL headers.")
+endif(OPENCL_INCLUDE_DIR-NOTFOUND)
 
 set(OPENCL_LIBRARIES ${OPENCL_LIBRARY})
 set(OPENCL_INCLUDE_DIRS ${OPENCL_INCLUDE_DIR})
